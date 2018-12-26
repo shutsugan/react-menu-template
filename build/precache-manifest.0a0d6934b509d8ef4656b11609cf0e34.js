6a5b309c84d6a041f9ff9107cfa2df51c7426562
shutsugan <muhammad@nextmedia.ma>
2018-12-26 14:49:54 +0100

    Initial commit from Create React App

---
+++ build/precache-manifest.0a0d6934b509d8ef4656b11609cf0e34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "148d836e05b7899d881f",
+    "url": "/static/js/main.148d836e.chunk.js"
+  },
+  {
+    "revision": "09695a900ff386892385",
+    "url": "/static/js/1.09695a90.chunk.js"
+  },
+  {
+    "revision": "148d836e05b7899d881f",
+    "url": "/static/css/main.dbfe8b1b.chunk.css"
+  },
+  {
+    "revision": "3e8b7421ef31af4ed1a0894294686aff",
+    "url": "/index.html"
+  }
+];
