e619deddd3001c8d8e8d7fab76c5edff74140b59
shutsugan <muhammad@nextmedia.ma>
2018-12-27 19:50:41 +0100

    refactor (app): fix some style rules and remove other rules

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import HeroBanner from '../HeroBanner';
+import NavCategories from '../NavCategories';
 import './index.css';
 
 const Home = _ => (
     <div className="home flex-center flex-row">
         <HeroBanner />
+        <NavCategories />
     </div>
 );
 
