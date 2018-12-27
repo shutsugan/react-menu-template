66692f8042e54cf748018b246f5d84717265dda6
shutsugan <muhammad@nextmedia.ma>
2018-12-27 15:16:55 +0100

    feat (navheader): create a new navhead wrapper component

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const NavHeader = ({logo, label}) => (
-    <div></div>
+import Logo from '../Logo';
+import NavItems from '../NavItems';
+import Search from '../Search';
+
+import './index.css';
+
+const NavHeader = _ => (
+    <div className="nav-header">
+        <Logo logo="oceanprime" label="Bar" />
+        <NavItems />
+        <Search />
+    </div>
 );
 
 export default NavHeader;
