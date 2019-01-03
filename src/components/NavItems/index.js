7d2cee55e942bf95ced321c5fb37c7a52ccb29ab
shutsugan <muhammad@nextmedia.ma>
2019-01-03 12:09:02 +0100

    feat (navitems): add anchor transition between navitems and body sections

--- src/components/NavItems/index.js
+++ src/components/NavItems/index.js
@@ -4,9 +4,10 @@ import './index.css';
 
 const NavItems = _ => (
   <nav className="nav-items flex-row">
-    <a className="nav__item" href="#about">About</a>
-    <a className="nav__item" href="#subscribe">Subscribe</a>
+    <a className="nav__item" href="#about">About</a>  
   </nav>
 );
+
+//<a className="nav__item" href="#subscribe">Subscribe</a>
 
 export default NavItems;
